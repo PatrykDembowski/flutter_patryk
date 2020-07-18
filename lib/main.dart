e1dc26aa60c818e6fd2a49d180b8dd25126ca236
Patryk_Dembowski <45976197+PatrykDembowski@users.noreply.github.com>
2020-07-18 23:40:40 +0200

    ndawanie funkcji przyciskom

--- lib/main.dart
+++ lib/main.dart
@@ -21,6 +21,27 @@ class SimpleCalculator extends StatefulWidget {
 }
 
 class _SimpleCalculatorState extends State<SimpleCalculator> {
+  String equation="0";
+  String result="0";
+  String expression="";
+  double equationFontSize=38.0;
+  double resultFontSize=48.0;
+  buttonPressed(String buttonText)
+  {
+setState(() {
+  if (buttonText == "C") {
+
+  } else if (buttonText == "⌫") {}
+  else if(buttonText=="=="){
+
+  }
+  else {
+    equation=equation+buttonText;
+
+
+  }
+  });
+  }
   Widget buildButton(String buttonText,double buttonHeight, Color buttonColor)
   {
     return Container(height: MediaQuery.of(context).size.height *0.1*buttonHeight,
@@ -31,7 +52,7 @@ class _SimpleCalculatorState extends State<SimpleCalculator> {
           side:BorderSide(color: Colors.white,width: 1,style: BorderStyle.solid)
       ),
           padding: EdgeInsets.all(16.0),
-          onPressed: null,
+          onPressed: ()=>buttonPressed(buttonText),
           child: Text(buttonText,
             style: TextStyle(
                 fontSize: 30.0,
@@ -53,13 +74,13 @@ class _SimpleCalculatorState extends State<SimpleCalculator> {
         children:<Widget>[
           Container(alignment:Alignment.centerRight ,
           padding: EdgeInsets.fromLTRB(10, 20, 20, 10),
-          child: Text("0", style:TextStyle(fontSize: 38.0)),),
+          child: Text(equation, style:TextStyle(fontSize: equationFontSize)),),
 
 
 
           Container(alignment:Alignment.centerRight ,
             padding: EdgeInsets.fromLTRB(10, 20, 20, 10),
-            child: Text("0", style:TextStyle(fontSize: 38.0)),),
+            child: Text(result, style:TextStyle(fontSize: resultFontSize)),),
           Expanded(
             child: Divider(),
           ),
@@ -98,9 +119,44 @@ class _SimpleCalculatorState extends State<SimpleCalculator> {
                         buildButton("3", 1, Colors.black54),
                       ]
                   ),
+                  TableRow(
+                      children: [
+                        buildButton(".", 1, Colors.black54),
+                        buildButton("0", 1, Colors.black54),
+                        buildButton("00", 1, Colors.black54),
+                      ]
+                  ),
               ],
                 ),
-          )
+          ),
+              Container(
+                width: MediaQuery.of(context).size.width*0.25,
+                child : Table(children: [
+                  TableRow(
+                    children:[
+                      buildButton("x", 1, Colors.blue),
+                    ]
+                  ),
+                  TableRow(
+                      children:[
+                        buildButton("-", 1, Colors.blue),
+                      ]
+                  ),
+                  TableRow(
+                      children:[
+                        buildButton("+", 1, Colors.blue),
+                      ]
+                  ),
+                  TableRow(
+                      children:[
+                        buildButton("=", 2, Colors.redAccent),
+                      ]
+                  ),
+
+    ],
+    ),
+                )
+
     ],
     ),
   ]),
